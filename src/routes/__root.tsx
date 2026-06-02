c4f19771e8244aaf08eab4b6f974c1086f62d39e
RubenCuevasQ <ruben.cuevas@sadhanacore.cl>
2026-06-02 15:01:47 -0400

    nuevasyprimerasmejorasprisma

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -95,6 +95,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/logo.png",
+      },
     ],
   }),
   shellComponent: RootShell,
