1b62f3ed7b09154e00b49224cebd85c0d1db38da
RubenCuevasQ <ruben.cuevas@sadhanacore.cl>
2026-06-02 15:09:42 -0400

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    preset: "vercel",
+    output: {
+      dir: ".output",
+      publicDir: ".output/static",
+      serverDir: ".output/server",
+    },
+  },
 });
